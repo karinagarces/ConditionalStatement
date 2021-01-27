fe7f791c8c2d6773281ae5ef0f4a21cc791c7f0d
Karina Garces <kgarces8@gmail.com>
2021-01-26 22:32:55 -0500

    Committing conditional statements

--- ConditionalStatement/Program.cs
+++ ConditionalStatement/Program.cs
@@ -6,7 +6,63 @@ namespace ConditionalStatement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What grade will you get?");
+            String userInput = Console.ReadLine(); // Get Input
+            int grade = int.Parse(userInput); // Convert to integer value
+            if (grade >= 98 )
+            {
+                Console.WriteLine("A+");
+            }
+            else if (grade >=92&& grade <98)
+            {
+                Console.WriteLine("A");
+            }
+            else if (grade >= 90 && grade < 92)
+            {
+                Console.WriteLine("A-");
+            }
+            else if (grade >= 88 && grade < 90)
+            {
+                Console.WriteLine("B+");
+            }
+            else if (grade >= 82 && grade < 88)
+            {
+                Console.WriteLine("B");
+            }
+            else if (grade >= 80 && grade < 82)
+            {
+                Console.WriteLine("B-");
+            }
+            else if (grade >= 78 && grade < 80)
+            {
+                Console.WriteLine("C+");
+            }
+            else if (grade >= 72 && grade < 78)
+            {
+                Console.WriteLine("C");
+            }
+            else if (grade >= 70 && grade < 72)
+            {
+                Console.WriteLine("C-");
+            }
+            else if (grade >= 68 && grade < 70)
+            {
+                Console.WriteLine("D+");
+            }
+            else if (grade >= 62 && grade < 68)
+            {
+                Console.WriteLine("D");
+            }
+            else if (grade >= 60 && grade < 62)
+            {
+                Console.WriteLine("D-");
+            }
+            else
+            {
+                Console.WriteLine("F");
+            }
+            Console.ReadKey(true);
+
         }
     }
 }
